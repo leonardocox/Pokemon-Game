d94c849819bda25aaada69d5fd80e22d4b333fa4
Leonardo Cox <leonardocx0@gmail.com>
2022-06-21 14:05:40 -0400

    finished proper cropping, width, and height for player and map

--- script.js
+++ script.js
@@ -8,10 +8,26 @@ canvas.height = 576;
 context.fillStyle = "white";
 context.fillRect(0, 0, canvas.width, canvas.height);
 
+// Making sure the sprite and map are loading in properly and at the right time
 const image = new Image();
 image.src = "./img/Tomo Valley Map Zoom.png";
-console.log(image);
+
+const playerImage = new Image();
+playerImage.src = "./img/playerDown.png";
 
 image.onload = () => {
-  context.drawImage(image, -500, -450);
+  context.drawImage(image, -450, -425);
+  context.drawImage(
+    playerImage,
+    // Cropping the player Image
+    0,
+    0,
+    playerImage.width / 4,
+    playerImage.height,
+    // Actual width and height
+    canvas.width / 2 - playerImage.width / 4 / 2,
+    canvas.height / 2 - playerImage.height / 2,
+    playerImage.width / 4,
+    playerImage.height
+  );
 };
