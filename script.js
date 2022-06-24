06dabe3d8b7cd3449bbd23afaa446c2009355833
Leonardo Cox <leonardocx0@gmail.com>
2022-06-24 15:24:12 -0400

    added classes file and Foreground Objects

--- script.js
+++ script.js
@@ -11,21 +11,6 @@ for (let i = 0; i < collisions.length; i += 70) {
   collisionsMap.push(collisions.slice(i, 70 + i));
 }
 
-class Boundary {
-  static width = 48;
-  static height = 48;
-  constructor({ position }) {
-    this.position = position;
-    this.width = 48;
-    this.height = 48;
-  }
-
-  draw() {
-    context.fillStyle = "rgba(255, 0, 0, 0)";
-    context.fillRect(this.position.x, this.position.y, this.width, this.height);
-  }
-}
-
 const boundaries = [];
 const offset = {
   x: -450,
@@ -50,36 +35,11 @@ collisionsMap.forEach((row, i) => {
 const image = new Image();
 image.src = "./img/Tomo Valley Map Zoom.png";
 
+const foregroundImage = new Image();
+foregroundImage.src = "./img/foregroundObject.png";
+
 const playerImage = new Image();
 playerImage.src = "./img/playerDown.png";
-
-class Sprite {
-  constructor({ position, velocity, image, frames = { max: 1 } }) {
-    this.position = position;
-    this.image = image;
-    this.frames = frames;
-
-    this.image.onload = () => {
-      this.width = this.image.width / this.frames.max;
-      this.height = this.image.height;
-    };
-  }
-  draw() {
-    context.drawImage(
-      this.image,
-      // Cropping the player Image
-      0,
-      0,
-      this.image.width / this.frames.max,
-      this.image.height,
-      // Actual width and height
-      this.position.x,
-      this.position.y,
-      this.image.width / this.frames.max,
-      this.image.height
-    );
-  }
-}
 
 const player = new Sprite({
   position: {
@@ -100,6 +60,14 @@ const background = new Sprite({
   image: image,
 });
 
+const foreground = new Sprite({
+  position: {
+    x: offset.x,
+    y: offset.y,
+  },
+  image: foregroundImage,
+});
+
 const keys = {
   w: {
     pressed: false,
@@ -115,7 +83,7 @@ const keys = {
   },
 };
 
-const movalbes = [background, ...boundaries];
+const movalbes = [background, ...boundaries, foreground];
 
 function rectangularCollision({ rectangle1, rectangle2 }) {
   return (
@@ -133,6 +101,7 @@ function animate() {
     boundary.draw();
   });
   player.draw();
+  foreground.draw();
 
   let moving = true;
   if (keys.w.pressed && lastKey === "w") {
